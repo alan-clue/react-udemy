32e85a4d13f25e9bab3042cccd2ae1023c32b33c
alan0131 <abe.trigger@gmail.com>
2018-10-31 14:40:16 +0900

    third

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 
 class App extends Component {
+  
   render() {
+    const users = [
+      {name: "Taro",age:32},
+      {name: "Hanako",age:27},
+      {name: "hana" ,age:7},
+    ]
     return (
-      <div><h1>React Test!</h1></div>
+      <div>
+        {
+          users.map((user,index) => {
+            return <User name={user.name} age={user.age} key={index} />
+          })
+        }
+      </div>
     );
+    
   }
+}
+const User = (props) => {
+  return <div>名前は{props.name}、年齢は{props.age}です</div>
+}
+
+User.propTypes = {
+ name: PropTypes.string,
+ age: PropTypes.number
 }
 
 export default App;
